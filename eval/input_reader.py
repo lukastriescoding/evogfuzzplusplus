9b219681c3b07da5eb4747416d86041add113d93
lulufka <166993936+lulufka@users.noreply.github.com>
2024-07-11 10:22:46 +0200

    mannwhitneyu-test

--- eval/input_reader.py
+++ eval/input_reader.py
@@ -1,29 +1,23 @@
-def get_lists(l: str, iterations: int, filename: str):
+def get_lists(l: str, iteration: int, filename: str):
     with open(filename, 'r') as file:
         lines = file.readlines()
 
-    if iterations > 9 or iterations < 0:
-        return None
-
-    our_list = []
-
     if l == 'stand':
-        our_list = lines[iterations][1:-2].split(', ')
+        our_list = lines[iteration][1:-2].split(', ')
     elif l == 'naive':
-        our_list = lines[iterations + 11][1:-2].split(', ')
+        our_list = lines[iteration + 11][1:-2].split(', ')
     elif l == 'impr':
-        our_list = lines[iterations + 22][1:-2].split(', ')
+        our_list = lines[iteration + 22][1:-2].split(', ')
     elif l == 'soph':
-        our_list = lines[iterations + 33][1:-2].split(', ')
+        our_list = lines[iteration + 33][1:-2].split(', ')
     elif l == 'ratio_soph':
-        our_list = lines[iterations + 44][1:-2].split(', ')
+        our_list = lines[iteration + 44][1:-2].split(', ')
     elif l == 'diff_exp':
-        our_list = lines[iterations + 55][1:-2].split(', ')
+        our_list = lines[iteration + 55][1:-2].split(', ')
     else:
         return None
 
     our_list_int = []
-
     for stri in our_list:
         our_list_int.append(int(stri))
 
---
+++ eval/mannwhitneyu_test.py
@@ -0,0 +1,73 @@
+from scipy import stats
+from input_reader import get_lists
+
+
+def mannwhitneyu_test(stand, other):
+    _, p_val = stats.mannwhitneyu(stand, other)
+    return p_val
+
+
+def main():
+    filename = "data/resultscompl.txt"
+    iterations = 10
+
+    for i in range(iterations):
+        stand = get_lists("stand", i, filename)
+        naive = get_lists("naive", i, filename)
+        impr = get_lists("impr", i, filename)
+        soph = get_lists("soph", i, filename)
+        ratio_soph = get_lists("ratio_soph", i, filename)
+        diff_exp = get_lists("diff_exp", i, filename)
+
+        p_val_naive = mannwhitneyu_test(diff_exp, naive)
+        p_val_impr = mannwhitneyu_test(diff_exp, impr)
+        p_val_soph = mannwhitneyu_test(diff_exp, soph)
+        p_val_stand = mannwhitneyu_test(diff_exp, stand)
+        p_val_ratio_soph = mannwhitneyu_test(diff_exp, ratio_soph)
+
+        cur_fitfunc = diff_exp
+        if p_val_naive < 0.05:
+            if sum(cur_fitfunc) < sum(naive):
+                p_val_naive = '\color{red}$\ding{55}$\color{black}' #cross
+            else:
+                p_val_naive = '\color{red}$\ding{51}$\color{black}' #checkmark
+        else:
+            p_val_naive = round(p_val_naive, 2)
+
+        if p_val_impr < 0.05:
+            if sum(cur_fitfunc) < sum(impr):
+                p_val_impr = '\color{red}$\ding{55}$\color{black}'  # cross
+            else:
+                p_val_impr = '\color{red}$\ding{51}$\color{black}'  # checkmark
+        else:
+            p_val_impr = round(p_val_impr, 2)
+
+        if p_val_soph < 0.05:
+            if sum(cur_fitfunc) < sum(soph):
+                p_val_soph = '\color{red}$\ding{55}$\color{black}'  # cross
+            else:
+                p_val_soph = '\color{red}$\ding{51}$\color{black}'  # checkmark
+        else:
+            p_val_soph = round(p_val_soph, 2)
+
+        if p_val_stand < 0.05:
+            if sum(cur_fitfunc) < sum(stand):
+                p_val_stand = '\color{red}$\ding{55}$\color{black}'  # cross
+            else:
+                p_val_stand = '\color{red}$\ding{51}$\color{black}'  # checkmark
+        else:
+            p_val_stand = round(p_val_stand, 2)
+
+        if p_val_ratio_soph < 0.05:
+            if sum(cur_fitfunc) < sum(ratio_soph):
+                p_val_ratio_soph = '\color{red}$\ding{55}$\color{black}'  # cross
+            else:
+                p_val_ratio_soph = '\color{red}$\ding{51}$\color{black}'  # checkmark
+        else:
+            p_val_ratio_soph = round(p_val_ratio_soph, 2)
+
+        print(i, '&', p_val_stand, '&', p_val_naive, '&', p_val_impr, '&', p_val_soph, '&', p_val_ratio_soph, "\\\\")
+
+
+if __name__ == "__main__":
+    main()
